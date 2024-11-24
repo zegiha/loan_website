56f1b01b679a42bf3be1435fc4858bbd4f8fce20
zegiha <leeseoyul070814@gmail.com>
2024-11-24 16:37:05 +0900

    style: page의 import 문 간소화는 위한 index.ts 도입

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import DisplaySection from "@/features/home/displaySection/DisplaySection";
-import PremiumBannerAndRealTimeLoanSection
-  from "@/features/home/PremiumBannerAndRealTimeLoanSection/PremiumBannerAndRealTimeLoanSection";
 import {Col} from "@/components/atoms/layout";
+import {
+  DisplaySection,
+  PremiumBannerAndRealTimeLoanSection,
+  MainRegisteredCompanySection,
+} from "@/features/home";
 
 export default function Home() {
   return (
     <Col style={{width: '100%'}}>
       <DisplaySection/>
       <PremiumBannerAndRealTimeLoanSection/>
+      <MainRegisteredCompanySection/>
     </Col>
   );
 }
---
+++ features/home/index.ts
@@ -0,0 +1,9 @@
+import DisplaySection from "@/features/home/displaySection/DisplaySection";
+import PremiumBannerAndRealTimeLoanSection from "@/features/home/PremiumBannerAndRealTimeLoanSection/PremiumBannerAndRealTimeLoanSection";
+import MainRegisteredCompanySection from "@/features/home/MainRegisteredCompanySection/MainRegisteredCompanySection";
+
+export {
+  DisplaySection,
+  PremiumBannerAndRealTimeLoanSection,
+  MainRegisteredCompanySection,
+}
