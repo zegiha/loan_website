30c22213e30c308c8f71017f68c97933afa11803
zegiha <leeseoyul070814@gmail.com>
2024-11-24 21:40:10 +0900

    feat: table 시스템 제작

---
+++ components/organisms/table/Table.tsx
@@ -0,0 +1,22 @@
+import {Col} from "@/components/atoms/layout";
+
+export default function Table({
+  head,
+  children,
+  className,
+}: {
+  head: React.ReactNode,
+  children: React.ReactNode,
+  className?: string;
+}) {
+  return (
+    <Col
+      width={'fill'}
+      gap={8}
+      className={className}
+    >
+      {head}
+      {children}
+    </Col>
+  );
+}
