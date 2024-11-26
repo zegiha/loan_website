1bd4bf1b1742d70ae8bf54f74b6e915eff96c16d
zegiha <leeseoyul070814@gmail.com>
2024-11-27 02:40:31 +0900

    feat: 아코디언 제목 추가

---
+++ components/molecules/AccordionSectionTitle/AccordionSectionTitle.tsx
@@ -0,0 +1,94 @@
+import {Col, Row} from "@/components/atoms/layout";
+import Typo from "@/components/atoms/typo/Typo";
+import {ArrowIcon} from "@/components/atoms/icons";
+import {useEffect, useRef, useState} from "react";
+import style from './accordionSectionTitle.module.scss';
+import CheckIcon from "@/components/atoms/icons/CheckIcon";
+
+interface IAccordionSectionTitle {
+  title: React.ReactNode | string;
+  accordionData: Array<string>;
+  activeAccordion: string;
+  measurement: string;
+  onAccordionActiveChange: (activeData: string) => void;
+  lastComment?: string;
+}
+
+export default function AccordionSectionTitle({
+  title,
+  accordionData,
+  activeAccordion,
+  measurement,
+  onAccordionActiveChange,
+  lastComment
+}: IAccordionSectionTitle) {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [accordionOpen, setAccordionOpen] = useState<boolean>(false);
+
+  const handleMouseLeave = () => setAccordionOpen(false);
+  useEffect(() => {
+    if(ref.current) {
+      ref.current.addEventListener('mouseleave', handleMouseLeave);
+    }
+    return () => {
+      ref.current?.removeEventListener('mouseleave', handleMouseLeave);
+    }
+  }, []);
+
+  return (
+    <Row
+      ref={ref}
+      alignItems={'center'}
+      gap={8}
+      className={style.sectionTitle}
+      onClick={() => {
+        setAccordionOpen(true)
+      }}
+    >
+      {title}
+      <Row gap={4} alignItems={'center'}>
+        <Typo.SubBody color={'dim'}>
+          {activeAccordion}
+          {measurement}
+        </Typo.SubBody>
+        <ArrowIcon
+          size={16}
+          color={'dim'}
+          deg={90}
+        />
+      </Row>
+      {accordionOpen && (
+        <Row
+          className={style.accordionContainer}
+        >
+          <Col
+            gap={8}
+            width={160}
+            className={style.accordionWrapper}
+          >
+            {accordionData.map((v, i) => (
+              <Row
+                key={v}
+                width={'fill'}
+                justifyContents={'space-between'}
+                className={style.accordion}
+                onClick={(e) => {
+                  onAccordionActiveChange(v)
+                }}
+              >
+                <Typo.Contents emphasize={v === activeAccordion}>
+                  {`${v}${measurement}`}
+                  {i === accordionData.length-1 && lastComment && lastComment}
+                </Typo.Contents>
+                <CheckIcon
+                  size={20}
+                  color={v === activeAccordion ? 'primary' : 'none'}
+                />
+              </Row>
+            ))}
+          </Col>
+        </Row>
+      )}
+    </Row>
+  );
+}
