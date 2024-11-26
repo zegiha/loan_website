81d6d0f3588f8e692dd57e4dbfc353a68b085cc6
zegiha <leeseoyul070814@gmail.com>
2024-11-27 02:40:31 +0900

    feat: 아코디언 제목 추가

---
+++ components/atoms/icons/CheckIcon.tsx
@@ -0,0 +1,8 @@
+import BaseIcon, {IIcon} from "@/components/atoms/icons/BaseIcon";
+
+export default function CheckIcon(props: IIcon) {
+  return <BaseIcon
+    iconKey={'check'}
+    {...props}
+  />
+}
