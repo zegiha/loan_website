0d4071a1d8f86cd5b479a8f49884e1d1c844022c
zegiha <leeseoyul070814@gmail.com>
2024-11-27 02:43:04 +0900

    feat: BaseButton커서, onClick 시 event 컨트롤 가능하게

--- components/atoms/layout/flex/type.ts
+++ components/atoms/layout/flex/type.ts
@@ -27,7 +27,7 @@ export interface IRowAndCol {
   style?: CSSProperties;
   children: React.ReactNode;
   ref?: React.MutableRefObject<HTMLDivElement | null>;
-  onClick?: () => void;
+  onClick?: (e?: MouseEvent) => void;
 }
 
 export interface IFlex extends IRowAndCol {
