49021bd40d7181ac39467049e66f53d33437caf6
zegiha <leeseoyul070814@gmail.com>
2024-11-28 15:27:43 +0900

    chore: 작업 환경 변경

--- components/atoms/layout/Divider.tsx
+++ components/atoms/layout/Divider.tsx
@@ -3,6 +3,6 @@ import {semantic} from "@/shared/color";
 export default function Divider({height}: {height?: number}) {
   return <div
     style={{width: '100%', height: height ? `${height}px` : '1px'}}
-    className={`${semantic.outlineDefault}`}
+    className={`${height ? semantic.surfaceSurfaceDim : semantic.outlineDefault}`}
   />
 }
--- components/molecules/inputs/index.ts
+++ components/molecules/inputs/index.ts
@@ -1,9 +1,11 @@
 import BaseButton from "./buttons/BaseButton";
 import iconButton from "./buttons/iconButton.module.scss"
+import button from './buttons/button.module.scss';
 import BaseTextInput from "@/components/molecules/inputs/textInputs/BaseTextInput";
 
 export {
   BaseButton,
   iconButton,
+  button,
   BaseTextInput,
 }
