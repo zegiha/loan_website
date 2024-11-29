ecca7ffa711c7f40e8bebb9c85dcc1831cc0bc20
zegiha <leeseoyul070814@gmail.com>
2024-11-29 10:16:34 +0900

    feat: LoanDetails 페이지 제작

--- app/loan/[postId]/page.tsx
+++ app/loan/[postId]/page.tsx
@@ -1,13 +1,13 @@
-import {Col, Divider, Row} from "@/components/atoms/layout";
-import {DetailsTitleSection} from "@/components/organisms";
-import {TPrimaryAndGenericColorString} from "@/shared/type";
-import DetailsContentsSection from "@/components/organisms/details/detailsContentsSection/DetailsContentsSection";
-import TextTable from "@/components/organisms/table/TextTable";
-import {TableRow} from "@/components/molecules";
-import TextTableRow from "@/components/molecules/table/TextTableRow";
-import Typo from "@/components/atoms/typo/Typo";
+'use server';
 
-export default function LoanDetails() {
+import {Col, Divider} from "@/components/atoms/layout";
+import {TPrimaryAndGenericColorString} from "@/shared/type";
+import CompanySection from "@/features/loanDetails/CompanySection";
+import ProductSection from "@/features/loanDetails/ProductSection";
+import AdditionalExplain from "@/features/loanDetails/AdditionalExplain";
+import {DetailsTitleSection} from "@/components/organisms";
+
+export default async function LoanDetails() {
   const title: TPrimaryAndGenericColorString = [
     {type: 'primary', contents: '無방문 '},
     {type: 'generic', contents: '24시 당일입금 비대면 정식등록업체 간편진행 24시 당일입금 비대면 정식등록업체 간편진행 24시 당일입금 비대면 정식등록업체 간편진행 24시 당일입금 비대면 정식등록업체 간편진행 24시 당일입금 비대면 정식등록업체 간편진행 24시 당일입금 비대면 정식등록업체 간편진행'}
@@ -37,6 +37,10 @@ export default function LoanDetails() {
           {title: '업체명', contents: '스마트 대부 중개'},
           {title: '업체명', contents: '스마트 대부 중개'},
           {title: '업체명', contents: '스마트 대부 중개'},
+          {title: '업체명', contents: '스마트 대부 중개'},
+          {title: '업체명', contents: '스마트 대부 중개'},
+          {title: '업체명', contents: '스마트 대부 중개'},
+          {title: '업체명', contents: '스마트 대부 중개'},
         ]
       },
     ],
@@ -46,36 +50,19 @@ export default function LoanDetails() {
     <Col width={'fill'}>
       <DetailsTitleSection type={'loan'} title={title}/>
       <Divider height={8}/>
-      <DetailsContentsSection subTitle={loanDetailsData.contents[0].subTitle ?? ''}>
-        <Row gap={16} width={'fill'}>
-          <Col width={'fill'}>
-            {loanDetailsData.contents[0].contents.map((v, i) => (
-              i === 3 ? (
-                <>
-                  <Divider key={i}/>
-                  <DetailsTextTableRow key={i} {...v}/>
-                </>
-              ) : (
-                <DetailsTextTableRow key={i} {...v}/>
-              )
-            ))}
-          </Col>
-        </Row>
-      </DetailsContentsSection>
+      <CompanySection
+        contents={loanDetailsData.contents[0].contents}
+      />
+      <Divider height={8}/>
+      <ProductSection
+        contents={loanDetailsData.contents[1].contents}
+      />
+      <Divider height={8}/>
+      <AdditionalExplain
+        contents={`haha\nhoho`}
+      />
     </Col>
   );
 }
 
-function DetailsTextTableRow({
-  title,
-  contents
-}: {title: string, contents: string}) {
-  return <TextTableRow>
-    <Typo.Contents width={120} color={'dim'}>
-      {title}
-    </Typo.Contents>
-    <Typo.Contents width={'fill'}>
-      {contents}
-    </Typo.Contents>
-  </TextTableRow>
-}
+
