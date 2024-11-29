38b5782f9bd32c7eeeab9032c66db6b3fec526b8
zegiha <leeseoyul070814@gmail.com>
2024-11-30 00:52:24 +0900

    refactor: SwiperPaginationAndNavigation 적용 및 Table 관련 코드 분리

---
+++ features/loanByLocation/ui/RegisteredCompanyTable.tsx
@@ -0,0 +1,46 @@
+import {TableHead, TableRow} from "@/components/molecules";
+import Typo from "@/components/atoms/typo/Typo";
+
+export function RegisteredCompanyTableHead() {
+  return <TableHead>
+    <Typo.Contents width={60}>
+      지역
+    </Typo.Contents>
+    <Typo.Contents width={80}>
+      대출한도
+    </Typo.Contents>
+    <Typo.Contents width={'fill'}>
+      제목
+    </Typo.Contents>
+    <Typo.Contents width={100}>
+      업체명
+    </Typo.Contents>
+  </TableHead>
+}
+
+export function RegisteredCompanyTableRow({
+  location,
+  loanLimit,
+  title,
+  name
+}: {
+  location: string,
+  loanLimit: string,
+  title: string,
+  name: string,
+}) {
+  return <TableRow>
+    <Typo.Contents width={60}>
+      {location}
+    </Typo.Contents>
+    <Typo.Contents width={80}>
+      {loanLimit}
+    </Typo.Contents>
+    <Typo.Contents width={'fill'} textOverflowLine={1}>
+      {title}
+    </Typo.Contents>
+    <Typo.Contents width={100} textOverflowLine={1}>
+      {name}
+    </Typo.Contents>
+  </TableRow>
+}
