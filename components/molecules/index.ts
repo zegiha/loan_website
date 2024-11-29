3f33afb15d183d3b1885b6364c6d863de2291659
zegiha <leeseoyul070814@gmail.com>
2024-11-29 10:16:10 +0900

    feat: TextTable 제작

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -3,6 +3,8 @@ import PremiumCard from "@/components/molecules/premiumCard/PremiumCard";
 import SectionServer from "@/components/molecules/section/Section";
 import TableHead from "@/components/molecules/table/TableHead";
 import TableRow from "@/components/molecules/table/TableRow";
+import Section from "@/components/molecules/section/Section";
+import TextTableRow from "@/components/molecules/table/TextTableRow";
 
 export {
   CompanyCard,
@@ -10,4 +12,6 @@ export {
   SectionServer,
   TableHead,
   TableRow,
+  Section,
+  TextTableRow,
 }
--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -2,10 +2,14 @@ import CompanyCardGrid from "@/components/organisms/companyCardGrid/CompanyCardG
 import PremiumBanner from "@/components/organisms/premiumBanner/PremiumBanner";
 import Table from "@/components/organisms/table/Table";
 import DetailsTitleSection from "@/components/organisms/details/detailsTitleSection/DetailsTitleSection";
+import DetailsContentsSection from "@/components/organisms/details/detailsContentsSection/DetailsContentsSection";
+import TextTable from "@/components/organisms/table/TextTable";
 
 export {
   CompanyCardGrid,
   PremiumBanner,
   Table,
-  DetailsTitleSection
+  DetailsTitleSection,
+  DetailsContentsSection,
+  TextTable,
 }
