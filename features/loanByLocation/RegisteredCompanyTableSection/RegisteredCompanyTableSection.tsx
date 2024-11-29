dbd1c5f39749e78c8e37cd8e248795e3b97b7a94
zegiha <leeseoyul070814@gmail.com>
2024-11-30 00:52:24 +0900

    refactor: SwiperPaginationAndNavigation 적용 및 Table 관련 코드 분리

--- features/loanByLocation/RegisteredCompanyTableSection/RegisteredCompanyTableSection.tsx
+++ features/loanByLocation/RegisteredCompanyTableSection/RegisteredCompanyTableSection.tsx
@@ -1,21 +1,21 @@
-import Section from "@/components/molecules/section/Section";
-import {useEffect, useRef, useState} from "react";
+'use client';
+
+import {useEffect, useState} from "react";
+import {Col, Row} from "@/components/atoms/layout";
+import {CloseIcon} from "@/components/atoms/icons";
 import Typo from "@/components/atoms/typo/Typo";
+import {Section, AccordionSectionTitle} from "@/components/molecules";
+import {BaseTextInput} from "@/components/molecules/inputs";
+import {SwiperPaginationAndNavigation, Table} from "@/components/organisms";
+import {TRegisterStatus} from "@/shared/type";
+import getRegisterCompany from "@/shared/api/getRegisterCompany";
 import {semantic} from "@/shared/color";
 import {formatActiveCategories} from "@/features/loanByLocation/helper";
-import AccordionSectionTitle from "@/components/molecules/AccordionSectionTitle/AccordionSectionTitle";
-import {Swiper, SwiperSlide} from "swiper/react";
-import type {Swiper as SwiperType} from 'swiper';
-import {TableHead, TableRow} from "@/components/molecules";
-import getRegisterCompany from "@/shared/api/getRegisterCompany";
-import {Table} from "@/components/organisms";
-import {TRegisterStatus} from "@/features/home/RegisterStatusTableAndRealTimeLoanTableSection/type";
-import {BaseButton, BaseTextInput, iconButton} from "@/components/molecules/inputs";
-import {Col, Row} from "@/components/atoms/layout";
-import {ArrowIcon} from "@/components/atoms/icons";
-import {eventSwcPlugins} from "next/dist/telemetry/events/swc-plugins";
-import {subscribeWithSelector} from "zustand/middleware/subscribeWithSelector";
-import CloseIcon from "@/components/atoms/icons/CloseIcon";
+import {SwiperSlide} from "swiper/react";
+import {
+  RegisteredCompanyTableHead,
+  RegisteredCompanyTableRow
+} from "@/features/loanByLocation/ui/RegisteredCompanyTable";
 
 const contentsNumberData = ['5', '10', '15', '20']
 
@@ -23,8 +23,6 @@ export default function RegisteredCompanyTableSection({
   activeCategories
 }: {activeCategories: Set<string>}) {
   const [activeContentsNumber, setActiveContentsNumber] = useState('5')
-
-  const swiperRef = useRef<SwiperType>();
 
   const formatRegisteredCompany = (rawData: Array<TRegisterStatus>): Array<Array<TRegisterStatus>> => {
     const res: Array<Array<TRegisterStatus>> = [];
@@ -35,64 +33,11 @@ export default function RegisteredCompanyTableSection({
     return res;
   }
   const [registeredCompanyData, setRegisteredCompanyData] = useState(formatRegisteredCompany(getRegisterCompany(60, Array.from(activeCategories), true)));
-
   const [search, setSearch] = useState<string>('');
-
-  const [activeSlides, setActiveSlides] = useState(1);
-  const [pagenation, setPagenation] = useState<Array<number>>([]);
-
-  const handlePagenationRender = () => {
-    if(swiperRef.current) {
-      const slideLength = swiperRef.current.slides.length;
-      const newPagenation: Array<number> = []
-      if(slideLength <= 5) {
-        for(let i = 1; i <= slideLength; i++)
-          newPagenation.push(i);
-      } else {
-        const start = activeSlides - 2;
-        const end = activeSlides + 2;
-        if(start <= 1) {
-          for(let i = 0; i < 5; i++)
-            newPagenation.push(i + 1);
-        } else {
-          if(end >= slideLength) {
-            for(let i = slideLength - 4; i <= slideLength; i++)
-              newPagenation.push(i);
-          } else {
-            for(let i = end - 4; i <= end; i++)
-              newPagenation.push(i);
-          }
-        }
-
-      }
-      setPagenation(newPagenation)
-    }
-  }
-
-  const handleNextSlide = () => {
-    if(swiperRef.current) {
-      if(swiperRef.current.allowSlideNext) {
-        swiperRef.current.slideNext(240);
-        setActiveSlides(swiperRef.current.activeIndex + 1);
-      }
-    }
-  }
-  const handlePrevSlide = () => {
-    if (swiperRef.current) {
-      if (swiperRef.current.allowSlidePrev) {
-        swiperRef.current.slidePrev(240);
-        setActiveSlides(swiperRef.current.activeIndex + 1);
-      }
-    }
-  }
 
   useEffect(() => {
     setRegisteredCompanyData([...formatRegisteredCompany(getRegisterCompany(60, Array.from(activeContentsNumber), true))])
   }, [activeContentsNumber]);
-
-  useEffect(() => {
-    handlePagenationRender()
-  }, [activeSlides]);
 
   return (
     <Section backgroundColor={'surface'}>
@@ -108,7 +53,7 @@ export default function RegisteredCompanyTableSection({
         accordionData={contentsNumberData}
         activeAccordion={activeContentsNumber}
         measurement={'개'}
-        onAccordionActiveChange={(newContentsNumber) => setActiveContentsNumber(newContentsNumber)}
+        onAccordionActiveChangeAction={(newContentsNumber) => setActiveContentsNumber(newContentsNumber)}
         lastComment={'( 최대 )'}
       />
       <Col gap={12} width={'fill'}>
@@ -131,27 +76,15 @@ export default function RegisteredCompanyTableSection({
             onChangeAction={(e) => setSearch(e.target.value)}
           />
         </Row>
-
-        <Swiper
-          onSwiper={(swiper) => {
-            swiperRef.current = swiper
-          }}
-          onUpdate={(swiper) => {
-            swiperRef.current = swiper
-            handlePagenationRender()
-            setActiveSlides(1)
-          }}
-          spaceBetween={40}
-          style={{
-            paddingLeft: 1,
-          }}
-        >
+        <SwiperPaginationAndNavigation>
           {registeredCompanyData.map((v, i) => (
-            <SwiperSlide key={i}>
-              <Table head={<RegisteredCompanyTableHead/>}>
-                {v.map((contents, i) => (
+            <SwiperSlide key={`${i}-slide`}>
+              <Table
+                head={<RegisteredCompanyTableHead/>}
+              >
+                {v.map((contents, index) => (
                   <RegisteredCompanyTableRow
-                    key={i}
+                    key={`${index}-tableItem`}
                     name={contents.companyName}
                     loanLimit={contents.loanLimit ?? '상담 후 결정'}
                     location={contents.location}
@@ -161,91 +94,8 @@ export default function RegisteredCompanyTableSection({
               </Table>
             </SwiperSlide>
           ))}
-        </Swiper>
-        <Row gap={8} width={'fill'} justifyContents={'center'}>
-          <BaseButton
-            className={`${iconButton.iconButton28}`}
-            onClick={() => handlePrevSlide()}
-          >
-            <ArrowIcon
-              size={20}
-              color={'dim'}
-              deg={180}
-            />
-          </BaseButton>
-          {pagenation.map((v, i) => (
-            <BaseButton
-              key={i}
-              className={`${v === activeSlides ? iconButton.iconButton28Active : iconButton.iconButton28}`}
-              onClick={() => {
-                swiperRef.current?.slideTo(v-1, 240)
-                setActiveSlides(v);
-              }}
-            >
-              <Typo.Contents color={activeSlides === v ? 'generic' : 'dim'}>
-                {v}
-              </Typo.Contents>
-            </BaseButton>
-          ))}
-          <BaseButton
-            className={`${iconButton.iconButton28}`}
-            onClick={() => handleNextSlide()}
-          >
-            <ArrowIcon
-              size={20}
-              color={'dim'}
-            />
-          </BaseButton>
-        </Row>
+        </SwiperPaginationAndNavigation>
       </Col>
     </Section>
   );
-}
-
-
-
-
-
-function RegisteredCompanyTableHead() {
-  return <TableHead>
-    <Typo.Contents width={60}>
-      지역
-    </Typo.Contents>
-    <Typo.Contents width={80}>
-      대출한도
-    </Typo.Contents>
-    <Typo.Contents width={'fill'}>
-      제목
-    </Typo.Contents>
-    <Typo.Contents width={100}>
-      업체명
-    </Typo.Contents>
-  </TableHead>
-}
-
-function RegisteredCompanyTableRow({
-  location,
-  loanLimit,
-  title,
-  name
-}: {
-  location: string,
-  loanLimit: string,
-  title: string,
-  name: string,
-}) {
-  return <TableRow>
-    <Typo.Contents width={60}>
-      {location}
-    </Typo.Contents>
-    <Typo.Contents width={80}>
-      {loanLimit}
-    </Typo.Contents>
-    <Typo.Contents width={'fill'} textOverflowLine={1}>
-      {title}
-    </Typo.Contents>
-    <Typo.Contents width={100} textOverflowLine={1}>
-      {name}
-    </Typo.Contents>
-  </TableRow>
 }
