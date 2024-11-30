1360491a1253fff3f9448033d770bf4c3ce3bb7d
zegiha <leeseoyul070814@gmail.com>
2024-11-30 12:23:52 +0900

    feat: modal 제작

---
+++ components/molecules/modal/Modal.tsx
@@ -0,0 +1,21 @@
+import {ReactNode} from "react";
+import {createPortal} from "react-dom";
+import style from './modal.module.scss';
+
+export default function Modal({
+  isOpen,
+  setIsOpen,
+  children
+}: {
+  isOpen: boolean,
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>,
+  children: ReactNode
+}) {
+  return isOpen ? createPortal(
+    <div className={style.modalContainer} onClick={() => setIsOpen(false)}>
+      <div className={style.modalWrapper}>
+        {children}
+      </div>
+    </div>
+    , document.getElementById('modal-root') as HTMLElement) : <></>;
+}
