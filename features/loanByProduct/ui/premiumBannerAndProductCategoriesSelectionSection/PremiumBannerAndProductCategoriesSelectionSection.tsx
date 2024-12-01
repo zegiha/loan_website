55869cbba595ee085e58b15d6e022be4648127f9
zegiha <leeseoyul070814@gmail.com>
2024-12-02 04:24:57 +0900

    feat: CategoryToggleButton 상태관리 컴포넌트에 추가, 상품별 업체 찾기 페이지 제작

---
+++ features/loanByProduct/ui/premiumBannerAndProductCategoriesSelectionSection/PremiumBannerAndProductCategoriesSelectionSection.tsx
@@ -0,0 +1,115 @@
+'use client'
+
+import {Section} from "@/components/molecules";
+import {Col, Row} from "@/components/atoms/layout";
+import {semantic} from "@/shared/color";
+import Typo from "@/components/atoms/typo/Typo";
+import {PremiumBanner} from "@/components/organisms";
+import {BaseTextInput, CategoryToggleButton} from "@/components/molecules/inputs";
+import {CloseIcon} from "@/components/atoms/icons";
+import React, {useEffect, useState} from "react";
+import style from './premiumBannerAndProductCategoriesSelectionSection.module.scss';
+import {TCategory} from "@/components/molecules/inputs/buttons/categoryToggleButton/CategoryToggleButton";
+
+const CATEGORIES: Array<TCategory> = [
+  {label: '전체', subLabel: 2037, active: true},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+  {label: '직장인대출', subLabel: 230, active: false},
+];
+
+export default function PremiumBannerAndProductCategoriesSelectionSection({
+  setActiveCategoriesAction
+}: {setActiveCategoriesAction: React.Dispatch<React.SetStateAction<Set<string>>>}) {
+  const [productSearch, setProductSearch] = useState<string>('');
+  const [categories, setCategories] = useState<Array<TCategory>>([...CATEGORIES]);
+
+  useEffect(() => {
+    const newActiveCategories: Set<string> = new Set();
+    categories.forEach((v) => {
+      if(v.active) {
+        newActiveCategories.add(v.label)
+      }
+    })
+    setActiveCategoriesAction(new Set(newActiveCategories));
+  }, [categories]);
+
+  return (
+    <Section>
+      <Row gap={24} width={'fill'}>
+        <Col gap={24} className={style.smallBox}>
+          <Typo.SubBody color={'variable'} emphasize>
+            <span className={semantic.onGenericOnGenericPrimary}>
+              프리미엄
+            </span>
+            대부업체
+          </Typo.SubBody>
+          <PremiumBanner defaultCardNumber={1}/>
+        </Col>
+        <Col gap={16} className={style.bigBox}>
+          <BaseTextInput
+            placeholder={'찾으시는 지역을 입력해주세요'}
+            size={'normal'}
+            width={'fill'}
+            maxWidth={280}
+            value={productSearch}
+            TypingIcon={
+            <div
+              style={{
+              width: 20, height: 20,
+              cursor: 'pointer',
+              }}
+              onClick={() => setProductSearch('')}
+            >
+              <CloseIcon size={20} color={'dim'}/>
+            </div>
+            }
+            onChangeAction={(e) => {
+              setProductSearch(e.target.value);
+            }}
+          />
+          <Row width={'fill'} gap={12} wrap>
+            {categories.map((v, i) => (
+              <CategoryToggleButton
+                key={`${v.label}-${i}`}
+                handleCategoryButtonValues={{
+                  i,
+                  v,
+                  defaultValue: CATEGORIES,
+                  setCategories
+                }}
+                active={v.active}
+                contents={v.label}
+                subContents={`${v.subLabel}개`}
+              />
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </Section>
+  );
+}
