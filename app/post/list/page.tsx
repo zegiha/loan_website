d96f7b3100e41548cc6c0d8d64d9674d978d2338
zegiha <leeseoyul070814@gmail.com>
2024-12-02 10:24:42 +0900

    feat: 실시간 대출 문의에 지역 및 대출 종류, 글 추가 및 종류 지정 글 입력 제작

---
+++ app/post/list/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import {Col} from "@/components/atoms/layout";
+import {useState} from "react";
+import CategorySelectionSection from "@/features/postList/ui/categorySelectionSection/CategorySelectionSection";
+import LoanPostTableSection from "@/features/postList/ui/loanPostTableSection/LoanPostTableSection";
+
+export default function PostList() {
+  const [activeLoanTypeCategories, setActiveLoanTypeCategories] = useState<Set <string>>(new Set);
+  const [activeLocationCategories, setActiveLocationCategories] = useState<Set<string>>(new Set);
+  return (
+    <Col width={'fill'}>
+      <CategorySelectionSection
+        setActiveLocationCategories={setActiveLocationCategories}
+        setActiveLoanTypeCategories={setActiveLoanTypeCategories}
+      />
+      <LoanPostTableSection
+        activeLoanTypeCategories={activeLoanTypeCategories}
+        activeLocationCategories={activeLocationCategories}
+      />
+    </Col>
+  );
+}
