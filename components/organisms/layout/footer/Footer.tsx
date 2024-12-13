b98a6996fb2325924a7e17bd2e3908d1fe9838ed
zegiha <leeseoyul070814@gmail.com>
2024-12-13 09:18:23 +0900

    feat: 풋터 반응형 대응

--- components/organisms/layout/footer/Footer.tsx
+++ components/organisms/layout/footer/Footer.tsx
@@ -14,14 +14,45 @@ export default async function Footer() {
         gap={24}
         className={style.footerContainer}
       >
-        <Image
-          src={LogoImage}
-          alt={'풋터 로고 이미지'}
-          width={213}
-          height={45}
-        />
+        <div className={style.section}>
+          <Image
+            src={LogoImage}
+            alt={'풋터 로고 이미지'}
+            width={213}
+            height={45}
+          />
+          <Col gap={12}>
+            <Typo.SubBody emphasize color={'variable'}>
+              고객센터
+            </Typo.SubBody>
+            <Col gap={8}>
+              <Col gap={4}>
+                <Typo.Contents emphasize>
+                  정보
+                </Typo.Contents>
+                <Typo.Contents>
+                  직통번호 : 010-8999-6968
+                </Typo.Contents>
+                <Typo.Contents>
+                  국민여행 773901-01-562759 강진우
+                </Typo.Contents>
+              </Col>
+              <Col gap={4}>
+                <Typo.Contents emphasize>
+                  영업시간
+                </Typo.Contents>
+                <Typo.Contents>
+                  09:00 ~ 18:00
+                </Typo.Contents>
+                <Typo.Contents>
+                  국경일 및 토, 일 휴무
+                </Typo.Contents>
+              </Col>
+            </Col>
+          </Col>
+        </div>
         <Col gap={12} width={'fill'}>
-          <Row gap={12} width={'fill'}>
+          <Row gap={32} width={'fill'} className={style.section}>
             <Typo.Contents width={'fill'} color={'variable'} isPre>
               사이트명 : 넷프로 대출정보{'\n'}
               상호명 : 넷프로{'\n'}
@@ -41,35 +72,6 @@ export default async function Footer() {
             COPYRIGHT ⓒ 2016. 넷프로 ALL RIGHTS RESERVED.
           </Typo.Contents>
         </Col>
-        <Col gap={12}>
-          <Typo.SubBody emphasize color={'variable'}>
-            고객센터
-          </Typo.SubBody>
-          <Col gap={8}>
-            <Col gap={4}>
-              <Typo.Contents emphasize>
-                정보
-              </Typo.Contents>
-              <Typo.Contents>
-                직통번호 : 010-8999-6968
-              </Typo.Contents>
-              <Typo.Contents>
-                국민여행 773901-01-562759 강진우
-              </Typo.Contents>
-            </Col>
-            <Col gap={4}>
-              <Typo.Contents emphasize>
-                영업시간
-              </Typo.Contents>
-              <Typo.Contents>
-                09:00 ~ 18:00
-              </Typo.Contents>
-              <Typo.Contents>
-                국경일 및 토, 일 휴무
-              </Typo.Contents>
-            </Col>
-          </Col>
-        </Col>
       </Row>
       <Col
         width={'fill'}
@@ -77,14 +79,14 @@ export default async function Footer() {
         className={style.footerContainer}
       >
         <Col>
-          <Typo.SubBody emphasize color={'variable'}>
-            넷프로에 기재된 광고내용은 게시자의 정보로써 이를 신뢰하여 취한 조치에 대해 어떠한 의무와 책임을 지지 않으며
-            넷프로는 직접적인 대출 및 중개를 하지 않습니다.
+          <Typo.SubBody emphasize color={'variable'} isPre>
+            넷프로에 기재된 광고내용은 게시자의 정보로써 이를 신뢰하여 취한 조치에 대해 어떠한 의무와 책임을 지지 않으며{'\n'}
+            넷프로는 직접적인 대출 및 중개를 하지 않습니다.{'\n'}
           </Typo.SubBody>
-          <Typo.Contents color={'dim'}>
-            상환기간 예시 : 모든 개인대출은 상환기간이 최단 60일이상, 최대 상환기간은 12개월(해당 업체 상의)
-            총 대출 예시 비용: 100만원을 12개월 기간동안 이자 최대연이율 24% 원리금균등상환 적용시 총상환금액 1,134,715원 | 채무의 조기상환수수료율 등 조기상환조건 없음.
-            금리 연24%이내, 연체이자율 24% 이내, 취급수수료 없음, 중도상환 수수료 없음, 중개수수료 없음, 추가비용 없음
+          <Typo.Contents color={'dim'} isPre>
+            상환기간 예시 : 모든 개인대출은 상환기간이 최단 60일이상, 최대 상환기간은 12개월(해당 업체 상의){'\n'}
+            총 대출 예시 비용: 100만원을 12개월 기간동안 이자 최대연이율 24% 원리금균등상환 적용시 총상환금액 1,134,715원 | 채무의 조기상환수수료율 등 조기상환조건 없음.{'\n'}
+            금리 연24%이내, 연체이자율 24% 이내, 취급수수료 없음, 중도상환 수수료 없음, 중개수수료 없음, 추가비용 없음{'\n'}
           </Typo.Contents>
         </Col>
         <Typo.SubBody emphasize>
