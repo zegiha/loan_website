d72ebd651e30cdd069c3ed73019a98362febdffc
zegiha <leeseoyul070814@gmail.com>
2024-12-30 22:52:23 +0900

    feat: Section_wrapper에 상단 네브바 적용

--- features/my/ui/Sidebar.tsx
+++ features/my/ui/Sidebar.tsx
@@ -11,6 +11,7 @@ const navigations: Array<TNavigation> = [
 	{domain: '/my/ads', name: '나의 광고'},
 	{domain: '/my/ads/new', name: '광고 추가하기'},
 	{domain: '/my/logout', name: '로그아웃'},
+  {domain: '/my/leave', name: '회원탈퇴'}
 ]
 
 export default function Sidebar() {
---
+++ features/my/ui/Topbar.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import {Row} from "@/components/atoms/layout";
+import {usePathname} from "next/navigation";
+import Typo from "@/components/atoms/typo/Typo";
+import Link from "next/link";
+import style from './style.module.scss';
+
+type TNavigation = {domain: string, name: string}
+const navigations: Array<TNavigation> = [
+  {domain: '/my/ads', name: '나의 광고'},
+  {domain: '/my/ads/new', name: '광고 추가하기'},
+  {domain: '/my/logout', name: '로그아웃'},
+  {domain: '/my/leave', name: '회원탈퇴'}
+]
+
+export default function Topbar() {
+  const pathname = usePathname();
+  return (
+    <Row className={style.topbar_container}>
+      <Row gap={24} style={{padding: 16}}>
+        {navigations.map((v, i) => (
+          <Link key={i} href={v.domain} style={{cursor: 'pointer'}}>
+            <Typo.Contents
+              color={pathname === v.domain ? 'primary' : 'generic'}
+            >
+              {v.name}
+            </Typo.Contents>
+          </Link>
+        ))}
+      </Row>
+    </Row>
+  );
+}
