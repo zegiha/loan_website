1f7459e97a6c5594ebddc3f5008b717d98892c18
zegiha <leeseoyul070814@gmail.com>
2024-12-30 23:25:28 +0900

    feat: 광고 추가하기의 각 광고 컴포넌트 추가

--- app/my/ads/new/page.tsx
+++ app/my/ads/new/page.tsx
@@ -1,3 +1,13 @@
-export default function My_ads_new() {
+import {Col} from "@/components/atoms/layout";
+import Section_wrapper from "@/features/my/ui/Section_wrapper";
+import New_ads from "@/features/my/new_ads/New_ads";
 
+export default function My_ads_new() {
+  return (
+    <Col width={'fill'}>
+      <Section_wrapper>
+        <New_ads/>
+      </Section_wrapper>
+    </Col>
+  );
 }
