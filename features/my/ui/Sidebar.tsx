0886066a7a93dcc21fc46fbd4612f680a9344e81
zegiha <leeseoyul070814@gmail.com>
2024-12-30 20:14:28 +0900

    feat: 내 계정 관리를 위한 Section_wrapper, sidebar 제작, 반응형 navBar 제작 미포함

---
+++ features/my/ui/Sidebar.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import {Col} from "@/components/atoms/layout";
+import Typo from "@/components/atoms/typo/Typo";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import style from "./style.module.scss";
+
+type TNavigation = {domain: string, name: string}
+const navigations: Array<TNavigation> = [
+	{domain: '/my/ads', name: '나의 광고'},
+	{domain: '/my/ads/new', name: '광고 추가하기'},
+	{domain: '/my/logout', name: '로그아웃'},
+]
+
+export default function Sidebar() {
+	const pathname = usePathname();
+	return (
+		<div className={style.sidebar_container}>
+			<Col gap={16} width={'fill'}>
+				<Typo.Body emphasize color={'variable'}>마이페이지</Typo.Body>
+				<Col gap={12} width={'fill'}>
+					{navigations.map((v) => (
+						<Link
+							key={v.name}
+							href={v.domain}
+							className={[
+								pathname !== v.domain ?
+									style.sidebar_item : style.sidebar_item_active,
+								pathname === v.domain && (v.name === '로그아웃' || v.name === '회원탈퇴') ?
+									style.sidebar_item_red : ''
+							].join(' ')}
+						>
+							<Typo.SubBody color={'dim'}>
+								{v.name}
+							</Typo.SubBody>
+						</Link>
+					))}
+				</Col>
+			</Col>
+		</div>
+	);
+}
