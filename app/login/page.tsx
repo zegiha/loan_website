01c54a8d4619e1e3d442f687dfee34697ce101ca
zegiha <leeseoyul070814@gmail.com>
2025-01-06 17:45:41 +0900

    feat: login페이지 register에 연결

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,12 +2,13 @@
 
 import React, {useState} from "react";
 import InputSection from "@/components/molecules/Layout/inputSection/InputSection";
-import {Col} from "@/components/atoms/layout";
+import {Col, Row} from "@/components/atoms/layout";
 import Typo from "@/components/atoms/typo/Typo";
 import {BaseButton, BaseTextInput, button} from "@/components/molecules/inputs";
 import {useRouter} from "next/navigation";
 import {use_auth_store} from "@/shared/store/authStore";
 import {login_action} from "@/shared/api/login_action";
+import Link from "next/link";
 
 export default function Login() {
   const [id, setId] = useState('')
@@ -31,30 +32,41 @@ export default function Login() {
 
   return (
     <InputSection title={'로그인'}>
-      <Col gap={4} width={'fill'}>
-        <Typo.Caption color={'dim'}>아이디</Typo.Caption>
-        <BaseTextInput
-          width={'fill'}
-          size={'normal'}
-          value={id}
-          onChangeAction={(v) => setId(v)}
-          placeholder={'아이디를 입력해주세요'}
-        />
+      <Col width={'fill'} gap={32}>
+        <Col width={'fill'} gap={16}>
+          <Col gap={4} width={'fill'}>
+            <Typo.Caption color={'dim'}>아이디</Typo.Caption>
+            <BaseTextInput
+              width={'fill'}
+              size={'normal'}
+              value={id}
+              onChangeAction={(v) => setId(v)}
+              placeholder={'아이디를 입력해주세요'}
+            />
+          </Col>
+          <Col gap={4} width={'fill'}>
+            <Typo.Caption color={'dim'}>비밀번호</Typo.Caption>
+            <BaseTextInput
+              width={'fill'}
+              inputType={'password'}
+              size={'normal'}
+              value={password}
+              onChangeAction={(v) => setPassword(v)}
+              placeholder={'비밀번호를 입력해주세요'}
+            />
+          </Col>
+        </Col>
+        <Col width={'fill'} gap={8}>
+          <BaseButton className={button.primary_button36} onClick={() => loginAction()}>
+            <Typo.SubBody emphasize color={'onPrimary'}>완료</Typo.SubBody>
+          </BaseButton>
+          <Row width={'fill'} justifyContents={'center'}>
+            <Link href={'/register'} onClick={() => console.log('무우빙')}>
+              <Typo.Caption color={'dim'} underline>아직 계정이 없으신가요? 회원가입하기</Typo.Caption>
+            </Link>
+          </Row>
+        </Col>
       </Col>
-      <Col gap={4} width={'fill'}>
-        <Typo.Caption color={'dim'}>비밀번호</Typo.Caption>
-        <BaseTextInput
-          width={'fill'}
-          inputType={'password'}
-          size={'normal'}
-          value={password}
-          onChangeAction={(v) => setPassword(v)}
-          placeholder={'비밀번호를 입력해주세요'}
-        />
-      </Col>
-      <BaseButton className={button.primary_button36} onClick={() => loginAction()}>
-        <Typo.SubBody emphasize color={'onPrimary'}>완료</Typo.SubBody>
-      </BaseButton>
     </InputSection>
   );
 }
