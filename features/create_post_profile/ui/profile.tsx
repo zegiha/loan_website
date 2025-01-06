48115a1a56c1b709068279dcdcf4f3d632508ede
zegiha <leeseoyul070814@gmail.com>
2025-01-06 17:45:22 +0900

    feat: shared로 phone_number_formatting 분리

--- features/create_post_profile/ui/profile.tsx
+++ features/create_post_profile/ui/profile.tsx
@@ -6,6 +6,7 @@ import {ArrowIcon} from "@/components/atoms/icons";
 import {usePost_data} from "@/features/create_post_profile/context/post_data_context";
 import React from "react";
 import style from './style.module.scss'
+import {formatting_phone_number} from "@/shared/helper";
 
 export default function Create_post_profile({
   setStep
@@ -18,30 +19,6 @@ export default function Create_post_profile({
     phone_number, setPhone_number,
     has_job, setHas_job,
   } = usePost_data()
-
-  const handle_phone_number = (text: string) => {
-    // 숫자만 추출
-    const onlyNumbers = text.replace(/[^0-9]/g, "");
-
-    // 최대 길이 제한 (11자리까지만 허용)
-    const trimmedNumbers = onlyNumbers.slice(0, 11);
-
-    // 번호 형식으로 실시간 변환
-    let formattedNumber = trimmedNumbers;
-    if (trimmedNumbers.length <= 3) {
-      formattedNumber = trimmedNumbers; // 010
-    } else if (trimmedNumbers.length <= 7) {
-      formattedNumber = trimmedNumbers.replace(/(\d{3})(\d{1,4})/, "$1-$2"); // 010-1234
-    } else {
-      formattedNumber = trimmedNumbers.replace(
-        /(\d{3})(\d{4})(\d{1,4})/,
-        "$1-$2-$3"
-      ); // 010-1234-5678
-    }
-
-    // 결과 저장
-    setPhone_number(formattedNumber);
-  };
 
   return (
     <InputSection title={'신원정보를 입력해주세요'}>
@@ -75,7 +52,7 @@ export default function Create_post_profile({
           width={'fill'}
           size={'normal'}
           value={phone_number}
-          onChangeAction={(v) => handle_phone_number(v)}
+          onChangeAction={(v) => setPhone_number(formatting_phone_number(v))}
           placeholder={'연락처를 입력해주세요'}
         />
       </Col>
---
+++ shared/helper/index.ts
@@ -0,0 +1,11 @@
+import formatting_phone_number from "@/shared/helper/formatting_phone_number";
+import is_correct_phone_number from "@/shared/helper/is_correct_phone_number";
+import is_correct_id from "@/shared/helper/is_correct_id";
+import is_correct_password from "@/shared/helper/is_correct_password";
+
+export {
+  formatting_phone_number,
+  is_correct_phone_number,
+  is_correct_id,
+  is_correct_password,
+}
