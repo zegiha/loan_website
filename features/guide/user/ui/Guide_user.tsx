9943186f189ad9c968de03a045057ede0a090c51
zegiha <leeseoyul070814@gmail.com>
2025-01-12 04:45:04 +0900

    feat: 이용안내 일반고객, 업체 제작

--- features/guide/user/ui/Guide_user.tsx
+++ features/guide/user/ui/Guide_user.tsx
@@ -1,4 +1,4 @@
-import {Col, Divider} from "@/components/atoms/layout";
+import {Col} from "@/components/atoms/layout";
 import React from "react";
 import First from "@/features/guide/user/ui/First";
 import Second from "@/features/guide/user/ui/Second";
@@ -7,7 +7,6 @@ export default function Guide_user() {
   return (
     <Col width={'fill'} gap={64}>
       <First/>
-      <Divider height={12}/>
       <Second/>
     </Col>
   )
