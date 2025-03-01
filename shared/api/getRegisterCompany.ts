158f49b7def5f14b48ca82c634f54a3e1354b9e0
zegiha <leeseoyul070814@gmail.com>
2025-03-01 15:52:39 +0900

    컴퓨터

--- shared/api/getRegisterCompany.ts
+++ shared/api/getRegisterCompany.ts
@@ -2,6 +2,7 @@ import {TRegisterStatus} from "@/shared/type";
 
 export default function getRegisterCompany(dataLength: number | undefined = 15, filter?: Array<string>, loanLimit?: boolean): Array<TRegisterStatus> {
   const dummy:TRegisterStatus = {
+    id: 'id',
     location: '인천',
     title: '200 대출문의드립니다 200 대출문의드립니다 200 대출문의드립니다 200 대출문의드립니다200 대출문의드립니다 200 대출문의드립니다 200 대출문의드립니다 200 대출문의드립니다200 대출문의드립니다 200 대출문의드립니다 200 대출문의드립니다 200 대출문의드립니다',
     companyName: '스피드 대출',
