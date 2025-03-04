7e3e58393855c343a9a13c99e1eee2496d55fde9
zegiha <leeseoyul070814@gmail.com>
2025-03-04 09:53:01 +0900

    to laptop

---
+++ shared/api/get_banner.ts
@@ -0,0 +1,37 @@
+'use server'
+
+import get_temp_image from "@/shared/api/get_temp_image";
+import {IBanner_data} from "@/shared/type";
+
+type page_option_type = 'home' | 'location' | 'product';
+
+function get_fetch_url(page_option: page_option_type): string {
+  switch(page_option) {
+    case "home": return "home_option_url"
+    case "location": return "location_option_url"
+    case "product": return "product_option_url"
+  }
+}
+
+const dummy: IBanner_data = {
+  title: "무방문 무서류 당일 대출",
+  subtitle: "지역 시간 장소 제약 X 월별 당일승인 당일송금",
+  name: "스피드 대출",
+  phone: "010-4612-4593",
+  location: "전국",
+  img_url: get_temp_image()
+}
+
+async function get_banner(
+  page_option: page_option_type,
+  data_number: number | undefined = 20
+): Promise<Array<IBanner_data>> {
+  const fetch_url = get_fetch_url(page_option);
+
+  let dummies: Array<IBanner_data> = [];
+  for(let i = 0; i < data_number; i++) dummies.push(dummy);
+
+  return dummies
+}
+
+export {get_banner};
--- shared/type/index.ts
+++ shared/type/index.ts
@@ -1,8 +1,10 @@
 import {TPrimaryAndGenericColorString} from "@/shared/type/primaryAndGenericColorString";
 import {TRegisterStatus, TRealTimeLoan} from "@/shared/type/loanCompanyAndPostTableData";
+import {IBanner_data} from "@/shared/type/IBanner";
 
 export type {
   TPrimaryAndGenericColorString,
   TRegisterStatus,
   TRealTimeLoan,
+  IBanner_data
 }
