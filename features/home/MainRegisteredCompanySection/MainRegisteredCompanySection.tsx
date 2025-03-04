824f2ce466b62d02d41762c44bae1f251f407a06
zegiha <leeseoyul070814@gmail.com>
2025-03-04 09:53:01 +0900

    to laptop

--- features/home/MainRegisteredCompanySection/MainRegisteredCompanySection.tsx
+++ features/home/MainRegisteredCompanySection/MainRegisteredCompanySection.tsx
@@ -1,21 +1,58 @@
-'use server'
+'use client'
 
-import getRegisteredCompanyWithImage from "@/shared/api/getRegisteredCompanyWithImage";
 import {CompanyCardGrid} from "@/components/organisms";
-import {CompanyCard} from "@/components/molecules";
 import Section from "@/components/molecules/Layout/section/Section";
+import {useFetch} from "@/shared/hooks";
+import {get_banner} from "@/shared/api";
+import {Banner} from "@/components/molecules";
+import {useEffect, useRef, useState} from "react";
+import {IBanner_data} from "@/shared/type";
 
-export default async function MainRegisteredCompanySection() {
+export default function MainRegisteredCompanySection() {
+  const [real_data, set_real_data] = useState<Array<IBanner_data> | null>(null);
+  const {data, is_loading, error, refetch} = useFetch(() => {
+    return get_banner('home')
+  })
+  const [target, set_target] = useState<HTMLDivElement | null>(null);
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if(entry.isIntersecting) {
+        refetch()
+      }
+    })
+  }, {
+    root: document.querySelector('#scrollArea'),
+    rootMargin: '0px',
+    threshold: 0.3,
+  })
+
+  useEffect(() => {
+    set_real_data(prev => {
+      if(prev === null) return data
+      else if(data === null) return prev
+      else return prev.concat(data)
+    })
+    if(target !== null) {
+      if(data !== null) {
+        observer.observe(target)
+      }
+      return () => {
+        observer.unobserve(target)
+      }
+    }
+  }, [data]);
+
   return (
     <Section backgroundColor={'surfaceDim'}>
       <CompanyCardGrid>
-        {getRegisteredCompanyWithImage({dummyN: 12, type: 'home'}).map((v, i) => (
-          <CompanyCard
+        {real_data !== null && real_data.map((v, i) => (
+          <Banner
             key={i}
-            type={'image'}
             {...v}
           />
         ))}
+        <div ref={set_target} style={{width: 0, height: 0}}/>
       </CompanyCardGrid>
     </Section>
   );
--- shared/hooks/index.ts
+++ shared/hooks/index.ts
@@ -1,5 +1,7 @@
 import usePaginationSwiper from "@/shared/hooks/usePaginationSwiper";
+import useFetch from "@/shared/hooks/useFetch";
 
 export {
   usePaginationSwiper,
+  useFetch
 }
