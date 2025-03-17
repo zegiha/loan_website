1c2057c4da8f99821cf41823b4a2280a94ae8747
zegiha <leeseoyul070814@gmail.com>
2025-03-17 17:47:04 +0900

    feat: 메인 레이아웃 수정, 퀵바 제작

---
+++ components/atoms/icons/Calculate_icon.tsx
@@ -0,0 +1,8 @@
+import BaseIcon, {IIcon} from "@/components/atoms/icons/BaseIcon";
+
+export default function Calculate_icon(props: IIcon) {
+	return <BaseIcon
+		iconKey={'calculate'}
+		{...props}
+	/>
+}
