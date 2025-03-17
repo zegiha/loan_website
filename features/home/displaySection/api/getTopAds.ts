b04bc61f5a893c21bbd64d8a2da15d8c27ac615e
zegiha <leeseoyul070814@gmail.com>
2025-03-17 17:47:04 +0900

    feat: 메인 레이아웃 수정, 퀵바 제작

--- features/home/displaySection/api/getTopAds.ts
+++ features/home/displaySection/api/getTopAds.ts
@@ -6,7 +6,6 @@ export interface ITopAd {
   title: TPrimaryAndGenericColorString;
   contents: string;
   name: string;
-  location: string;
   imgUrl: string;
 }
 
@@ -14,10 +13,9 @@ export default async function getTopAds(): Promise<Array<ITopAd>> {
   const res: Array<ITopAd> = [];
   for(let i = 0; i < 3; i++) {
     res.push({
-      title: [{type: 'primary', contents: 'TOP 메인 '}, {type: 'generic', contents: '광고주를 모십니다!'}],
-      contents: 'TOP 메인 광고주를 모십니다!\n1670-2962로 문의주세요!',
+      title: [{type: 'primary', contents: '무방문 무서류 당일 대출'}],
+      contents: '지역 시간 장소 제약 X 월별 당일승인 당일송금',
       imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlROcXWBsxzaZwXERUSfV6eD92_-KLFAvjbg&s',
-      location: '전국',
       name: '넷프로 대출',
     })
   }
