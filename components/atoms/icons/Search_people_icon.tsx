8ad731f12b2f6ed6a077d576f825d1f26cfa01ad
zegiha <leeseoyul070814@gmail.com>
2025-03-17 17:47:04 +0900

    feat: 메인 레이아웃 수정, 퀵바 제작

---
+++ components/atoms/icons/Search_people_icon.tsx
@@ -0,0 +1,8 @@
+import BaseIcon, {IIcon} from "@/components/atoms/icons/BaseIcon";
+
+export default function Search_people_icon(props: IIcon) {
+	return <BaseIcon
+		iconKey={'data_loss_prevention'}
+		{...props}
+	/>
+}
