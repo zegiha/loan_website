4090f79d73a6dfe39346ccbf458fc4e8c02285ba
zegiha <leeseoyul070814@gmail.com>
2025-03-20 20:23:52 +0900

    feat: 광고 수정, 연장 모달 온오프와 상태 설정

--- features/my/ads/ui/Add_contents.tsx
+++ features/my/ads/ui/Add_contents.tsx
@@ -1,4 +1,6 @@
-import {Col, Row} from "@/components/atoms/layout";
+'use client'
+
+import {Col} from "@/components/atoms/layout";
 import style from './style.module.scss';
 import Typo from "@/components/atoms/typo/Typo";
 import {Table} from "@/components/organisms";
@@ -6,27 +8,41 @@ import {
 	My_ads_table_head,
 	My_ads_table_row
 } from "@/features/my/ads/ui/My_adds_table";
+import Modal from "@/components/molecules/modal/Modal";
+import {useState} from "react";
 
 export default function Add_contents() {
 	const now = new Date();
+	const [is_edit_open, set_is_edit_open] = useState(false);
+	const [is_prolongation_open, set_is_prolongation_open] = useState(false);
 	return (
-		<Col width={'fill'} gap={16}>
-			<Typo.Body color={'variable'} emphasize>
-				나의 광고
-			</Typo.Body>
-			<Col width={'fill'} gap={4}>
-				<Table
-					className={style.table}
-					head={<My_ads_table_head/>}
-				>
-					<My_ads_table_row
-						add_type={'VIP 배너'}
-						title={'전라 지역 당일 대출'}
-						views={'139'}
-						registered_date={now}
-					/>
-				</Table>
+		<>
+			<Col width={'fill'} gap={16}>
+				<Typo.Body color={'variable'} emphasize>
+					나의 광고
+				</Typo.Body>
+				<Col width={'fill'} gap={4}>
+					<Table
+						className={style.table}
+						head={<My_ads_table_head/>}
+					>
+						<My_ads_table_row
+							add_type={'VIP 배너'}
+							title={'전라 지역 당일 대출'}
+							views={'139'}
+							registered_date={now}
+							edit_action={() => {set_is_edit_open(true)}}
+							prolongation_action={() => {set_is_prolongation_open(true)}}
+						/>
+					</Table>
+				</Col>
 			</Col>
-		</Col>
+			<Modal isOpen={is_edit_open} setIsOpen={set_is_edit_open}>
+				{/*TODO 수정창 만들기*/}
+			</Modal>
+			<Modal isOpen={is_prolongation_open} setIsOpen={set_is_prolongation_open}>
+				{/*TODO 광고연장창 만들기*/}
+			</Modal>
+		</>
 	);
 }
