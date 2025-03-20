4834e4257c1b953c31d397a704c2948dbcc752e2
zegiha <leeseoyul070814@gmail.com>
2025-03-20 12:41:40 +0900

    design: 배너 보는 개수 선택 제거

--- features/loanByLocation/RegisteredCompanyCardSection/RegisteredCompanyCardSection.tsx
+++ features/loanByLocation/RegisteredCompanyCardSection/RegisteredCompanyCardSection.tsx
@@ -1,8 +1,6 @@
 import {Banner} from "@/components/molecules";
 import Typo from "@/components/atoms/typo/Typo";
 import {semantic_object} from "@/shared/color";
-import {useState} from "react";
-import AccordionSectionTitle from "@/components/molecules/AccordionSectionTitle/AccordionSectionTitle";
 import {CompanyCardGrid} from "@/components/organisms";
 import {formatActiveCategories} from "@/features/loanByLocation/helper";
 import Section from "@/components/molecules/Layout/section/Section";
@@ -12,8 +10,6 @@ import dynamic from "next/dynamic";
 import load from '@/public/assets/load_dot_120.json'
 import {Row} from "@/components/atoms/layout";
 
-const contentsNumberData = ['10', '20', '30', '60']
-
 const Player = dynamic(
   () => import('@lottiefiles/react-lottie-player').then(m => m.Player),
   {ssr: false}
@@ -22,26 +18,16 @@ const Player = dynamic(
 export default function RegisteredCompanyCardSection({
   activeCategories
 }: {activeCategories: Set<string>}) {
-  const [activeContentsNumber, setActiveContentsNumber] = useState('20');
   const {data, is_loading} = useFetch(() => get_company_banner('location'))
 
   return (
     <Section>
-      <AccordionSectionTitle
-        title={
-          <Typo.Body emphasize color={'variable'}>
+      <Typo.Body emphasize color={'variable'}>
           <span style={{color: semantic_object.onGeneric.onGenericPrimary}}>
             {formatActiveCategories(activeCategories)}
           </span>
-            등록업체
-          </Typo.Body>
-        }
-        accordionData={contentsNumberData}
-        activeAccordion={activeContentsNumber}
-        measurement={'개'}
-        onAccordionActiveChangeAction={(newContentsNumber) => setActiveContentsNumber(newContentsNumber)}
-        lastComment={'( 최대 )'}
-      />
+        등록업체
+      </Typo.Body>
       {is_loading ? (
         <Row width={'fill'} justifyContents={'center'}>
           <Player src={load} autoplay loop style={{height: 24}}/>
