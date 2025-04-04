cbfdbb44c6bbac996513cef1d6208b19d5bd0332
zegiha <leeseoyul070814@gmail.com>
2025-04-04 18:34:09 +0900

    더미 텍스트 수정

--- features/home/displaySection/api/getTopAds.ts
+++ features/home/displaySection/api/getTopAds.ts
@@ -13,7 +13,7 @@ export default async function getTopAds(): Promise<Array<ITopAd>> {
   const tmp: Omit<ITopAd, 'imgUrl'> = {
     title: [{type: 'primary', contents: '무방문 무서류 당일 대출'}],
     contents: '지역 시간 장소 제약 X 월별 당일승인 당일송금',
-    name: '넷프로 대출',
+    name: '대부중개',
   }
   for(let i = 0; i < 3; i++) {
     res.push({
