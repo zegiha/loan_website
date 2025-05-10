9d37a1304f6aec8ca0c39e5a613af59bd4f2ae75
zegiha <leeseoyul070814@gmail.com>
2025-05-10 17:48:08 +0900

    옮김

--- app/post/create/page.tsx
+++ app/post/create/page.tsx
@@ -1,15 +1,37 @@
 'use client'
 
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {IPost_data, Post_data_context} from "@/features/create_post_profile/context/post_data_context";
 import Create_post_profile from "@/features/create_post_profile/ui/profile";
 import Create_post_post from "@/features/create_post_profile/ui/post";
 import Create_post_loan from "@/features/create_post_profile/ui/loan";
 import {useRouter} from "next/navigation";
+import axios from "axios";
 
+const upload = async () => {
+  try {
+    const res = await axios.post('https://loan.apne2a.algorix.cloud/loanboard', {
+      type: '신용',
+      available_location: '서울',
+      desired_amount: 10000,
+      age: 30,
+      gender: 'male',
+      tel: '01012345678',
+      monthly_income: 100000,
+      job_status: true,
+      title: 'tttt',
+      content: 'haha'
+    })
+    console.log(`success: ${res}`)
+    return true
+  } catch (e) {
+    console.error(e)
+    return false
+  }
+}
 
 export default function Create_post() {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(2);
   const [gender, setGender] = useState<'MALE' | 'FEMALE'>("MALE");
   const [age, setAge] = useState<string>('');
   const [phone_number, setPhone_number] = useState<string>('');
@@ -31,7 +53,9 @@ export default function Create_post() {
   const router = useRouter()
 
   useEffect(() => {
-    if(step >= 3) {
+
+    if(step === 3) {
+    } else if(step > 3) {
       router.push('/')
       setStep(2);
     }
@@ -39,9 +63,7 @@ export default function Create_post() {
 
   return (
     <Post_data_context.Provider value={defaultValue}>
-      {step < 3 && (
-        <Switcher step={step} setStep={setStep}/>
-      )}
+      <Switcher step={step} setStep={setStep}/>
     </Post_data_context.Provider>
   );
 }
@@ -52,6 +74,9 @@ function Switcher({
   switch (step) {
     case 0: return <Create_post_profile setStep={setStep} />
     case 1: return <Create_post_loan setStep={setStep}/>
-    case 2: return <Create_post_post setStep={setStep}/>
+    case 2: return <Create_post_post
+      test={upload}
+      setStep={setStep}
+    />
   }
 }
---
+++ shared/axios/lib/customInstance.ts
@@ -0,0 +1,48 @@
+// import {globalRouter} from '@/shared/lib'
+import axios, {AxiosError, AxiosRequestConfig} from 'axios'
+// import {adminControllerRefresh} from "@/entities/api/admin/admin";
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  withCredentials: true,
+})
+
+instance.interceptors.response.use(
+  res => res,
+  async (err) => {
+    if(err instanceof AxiosError && err.status === 401) {
+      try {
+        // globalRouter?.push('/login')
+        // await adminControllerRefresh()
+        return Promise.reject(err)
+      } catch (e) {
+        // if(e instanceof AxiosError && e.status === 401)
+      }
+    }
+  }
+)
+
+export const customInstance = <T>(
+  config: AxiosRequestConfig,
+  options?: AxiosRequestConfig,
+): Promise<T> => {
+  const source = axios.CancelToken.source()
+  const promise = instance({
+    ...config,
+    ...options,
+    cancelToken: source.token,
+  }).then(({data}) => data)
+
+  // @ts-ignore
+  promise.cancel = () => {
+    source.cancel('Query was cancelled')
+  };
+
+  return promise
+};
+
+export type ErrorType<Error> = AxiosError<Error>
+
+export type BodyType<BodyData> = BodyData
+
+export default instance
