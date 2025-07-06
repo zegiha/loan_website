b62dabcbad5071429f92a503423ae0f8e285ec4d
zegiha <leeseoyul070814@gmail.com>
2025-07-06 22:01:13 +0900

    feat: /to/site api 연결 및 redirection

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import {NextRequest, NextResponse} from "next/server";
 import {commonControllerLogVisitor} from "@/entities/api/common/common";
+import {customInstance} from "@/shared/axios/lib/customInstance";
 
 export async function middleware(
 	req: NextRequest,
@@ -8,15 +9,33 @@ export async function middleware(
 
   countVisitor()
 
+  if(pathname.endsWith("/to/site")) {
+    return toSiteRedirection(pathname)
+  }
+
 	if(pathname.startsWith('/api')) {
 		return NextResponse.next()
 	}
 }
 
 export const config = {
-	matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)'
+	matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/to/site',
+  ]
 }
 
 function countVisitor() {
   commonControllerLogVisitor()
+}
+
+async function toSiteRedirection(pathname: string) {
+  try {
+    await customInstance<void>(
+      {url: '/to/site'}
+    )
+    return NextResponse.redirect(pathname.slice(0, pathname.length-8))
+  } catch(e) {
+    return NextResponse.next()
+  }
 }
